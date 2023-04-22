9821d217d173d02ca697cf89b787e15e4ece643f
Michael Nikolaev <esp12p@gmail.com>
2023-04-22 23:35:00 +0300

    Create main code for robot

--- src/programs/code/robot/robot.ino
+++ src/programs/code/robot/robot.ino
@@ -1,12 +1,3 @@
-#include <GyverMotor.h>
-#include <GyverMotor2.h>
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  Serial.println("lol");
 }
