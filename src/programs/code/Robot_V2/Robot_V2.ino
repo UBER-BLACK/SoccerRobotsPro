002080bb0e0b2db1886fc8ba28e2f7791842557c
Michael Nikolaev <esp12p@gmail.com>
2023-04-24 14:51:13 +0300

    fix bug

--- src/programs/code/Robot_V2/Robot_V2.ino
+++ src/programs/code/Robot_V2/Robot_V2.ino
@@ -74,7 +74,7 @@ void setup_pc_monitor() {
 }
 void setup_motor_driver() { //CHECK HIGH / LOW
   Serial.println("Motor_Driver: Initialization...");
-  
+  pinMode(MotorPower,OUTPUT);
   //INSTALLING MOTOR DEADTIME
   Serial.println("Motor_Driver: Installing motor deadtime");
   MotorA.setDeadtime(Motor_Deadtime);
@@ -100,7 +100,7 @@ void setup_motor_driver() { //CHECK HIGH / LOW
   MotorC.setMinDuty(Motor_MinPower);
   //POWER AUTOSTART
   Serial.println("Motor_Driver: Power autostart");
-  
+  digitalWrite(MotorPower,HIGH);
   Serial.println("Motor_Driver: OK");
 }
 
